bc1ba1df09d13ab37c0247cc59f82ac087c08065
Yoan Ancelly <git@nizrod.ovh>
2024-07-28 19:07:40 +0200

    chore: enable debug in example

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,7 +4,7 @@ import { startApp } from "./app";
 console.log("Running gardisto check...");
 
 // Run the gardisto check
-gardisto({ debug: false });
+gardisto({ debug: true });
 
 // If gardisto doesn't exit the process, it means no errors were found
 console.log("No environment variable issues found. Starting the application.");
