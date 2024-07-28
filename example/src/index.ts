0871571e4b3ca7256379df49e4202ed82477f072
Yoan Ancelly <git@nizrod.ovh>
2024-07-28 17:37:51 +0200

    chore: update example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,10 +1,11 @@
-import { checkEnvVariables } from "gardisto";
+import { gardisto } from "gardisto";
 import { startApp } from "./app";
 
 console.log("Running gardisto check...");
 
 // Run the gardisto check
-checkEnvVariables({ debug: false });
+gardisto({ debug: false });
 
-// Start the application
+// If gardisto doesn't exit the process, it means no errors were found
+console.log("No environment variable issues found. Starting the application.");
 startApp();
