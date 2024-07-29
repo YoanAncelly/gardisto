ae9d298108aa6f3d462043d08e276cb37846e5e4
Yoan Ancelly <yoan.a@sonup.fr>
2024-07-29 12:12:11 +0200

    update example/src/index.ts

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,7 +4,11 @@ import { startApp } from "./app";
 console.log("Running gardisto check...");
 
 // Run the gardisto check
-gardisto({ debug: true });
+gardisto({
+  debug: true,
+  include: ["**/*.ts"],
+  exclude: ["node_modules/**", "dist/**", "tests/**"]
+});
 
 // If gardisto doesn't exit the process, it means no errors were found
 console.log("No environment variable issues found. Starting the application.");
